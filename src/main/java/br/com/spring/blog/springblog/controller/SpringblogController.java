abfb1356d11707d7cf2610203261b254e1dc4d72
Gabriel Sales <salesgabriel311@gmail.com>
2020-07-21 13:38:52 -0300

    Adicionando Security

--- src/main/java/br/com/spring/blog/springblog/controller/SpringblogController.java
+++ src/main/java/br/com/spring/blog/springblog/controller/SpringblogController.java
@@ -4,10 +4,15 @@ import br.com.spring.blog.springblog.model.Post;
 import br.com.spring.blog.springblog.service.SpringblogService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import javax.validation.Valid;
+import java.time.LocalDate;
 import java.util.List;
 
 @Controller
@@ -31,5 +36,25 @@ public class SpringblogController {
         mv.addObject("post", post);
         return mv;
     }
+
+    @GetMapping(value = "/newpost")
+    public String getPostForm(){
+        return "postForm";
+    }
+
+    @PostMapping(value = "/newpost")
+    public String savePost(@Valid Post post, BindingResult result, RedirectAttributes attributes) {
+        if(result.hasErrors()) {
+            attributes.addFlashAttribute("mensagem", "Verifique se os campos obrigatorios foram preenchidos!");
+            return "redirect:/newpost";
+        }
+
+        post.setData(LocalDate.now());
+        springblogService.save(post);
+        return "redirect:/posts";
+    }
+
+
+
 
 }
